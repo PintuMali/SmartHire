ae0f6c4f61b2ee5698aa7fe446665bd57ba7417e
Pintoo Mali <pintoomali05@gmail.com>
2022-12-20 21:40:30 +0530

    job-details page structure designed and searchbar added to the job page

--- src/app/employee/cities.service.ts
+++ src/app/employee/cities.service.ts
@@ -4,13 +4,13 @@ import { Cities } from "./cities.model";
 @Injectable({providedIn:'root'})
 export class CitiesService{
   private _cities:Cities[]=[
-    new Cities('c1','Mumbai','../../assets/images/boyonwork.svg'),
-    new Cities('c2','Chennei','../../assets/images/boyonwork.svg'),
-    new Cities('c3','Kolkata','../../assets/images/boyonwork.svg'),
-    new Cities('c4','Banglore','../../assets/images/boyonwork.svg'),
-    new Cities('c5','Hyderabad','../../assets/images/boyonwork.svg'),
-    new Cities('c6','Delhi','../../assets/images/boyonwork.svg'),
-    new Cities('c7','Jaipur','../../assets/images/boyonwork.svg')
+    new Cities('ci1','Mumbai','../../assets/images/boyonwork.svg'),
+    new Cities('ci2','Chennei','../../assets/images/boyonwork.svg'),
+    new Cities('ci3','Kolkata','../../assets/images/boyonwork.svg'),
+    new Cities('ci4','Banglore','../../assets/images/boyonwork.svg'),
+    new Cities('ci5','Hyderabad','../../assets/images/boyonwork.svg'),
+    new Cities('ci6','Delhi','../../assets/images/boyonwork.svg'),
+    new Cities('ci7','Jaipur','../../assets/images/boyonwork.svg')
   ];
   constructor(){
   }
--- src/app/employee/jobs/jobs.page.ts
+++ src/app/employee/jobs/jobs.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Jobs } from '../job.model';
+import { JobService } from '../job.service';
 
 @Component({
   selector: 'app-jobs',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jobs.page.scss'],
 })
 export class JobsPage implements OnInit {
+  months:string[]=['January','February','March','April','May','June','July','August','September','October','November','December']
+  hasSearchBarOpen:boolean=false;
 
-  constructor() { }
+  jobs:Jobs[];
+  constructor(private jobService:JobService,private router:Router) {
+    this.jobs=jobService.jobs;
+    for(let jobs of this.jobs){
+      let receivedDate=new Date(jobs.applybefore)
+      let timeDifference=receivedDate.getTime()-new Date().getTime();
+      let dateDifference=receivedDate.getDate()-new Date().getDate();
+      if(timeDifference>604800000){
+        jobs.hasEnoughTime=true;
+      jobs.applybefore=receivedDate.getDate().toString()+" "+this.months[receivedDate.getMonth().toString()];
+      console.log(dateDifference);
+
+      }
+      else{
+        jobs.hasEnoughTime=false;
+        jobs.applybefore=dateDifference.toString();
+      }
+    }
+   }
 
   ngOnInit() {
   }
+  onSearchBarIconClick(){
+    this.hasSearchBarOpen=true;
 
+  }
+  onCancelClick(e:any){
+    this.hasSearchBarOpen=false;
+
+  }
+  onApply(jobId:string){
+    this.router.navigateByUrl('/employee/jobs/'+jobId)
+  }
 }
