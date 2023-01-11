1c821539b3c86cb089d621cb9dfc71e846761924
Pintoo Mali <pintoomali05@gmail.com>
2023-01-11 19:17:25 +0530

    resolving conflicts6

--- src/app/employee/jobs/jobs.page.ts
+++ src/app/employee/jobs/jobs.page.ts
@@ -1,38 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Jobs } from '../job.model';
-import { JobService } from '../job.service';
+import { Subscription } from 'rxjs';
+import { Job } from '../job.model';
+import { JobsService } from '../job.service';
+
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.page.html',
   styleUrls: ['./jobs.page.scss'],
 })
-export class JobsPage implements OnInit {
+export class JobsPage implements OnInit,OnDestroy {
   months:string[]=['January','February','March','April','May','June','July','August','September','October','November','December']
   hasSearchBarOpen:boolean=false;
+  hasEnoughTime=true;
+  jobs:Job[];
+  isLoading=false;
+  private jobsSub:Subscription;
+  constructor(private jobsService:JobsService,private router:Router) {
 
-  jobs:Jobs[];
-  constructor(private jobService:JobService,private router:Router) {
-    this.jobs=jobService.jobs;
-    for(let jobs of this.jobs){
-      let receivedDate=new Date(jobs.applybefore)
-      let timeDifference=receivedDate.getTime()-new Date().getTime();
-      let dateDifference=receivedDate.getDate()-new Date().getDate();
-      if(timeDifference>604800000){
-        jobs.hasEnoughTime=true;
-      jobs.applybefore=receivedDate.getDate().toString()+" "+this.months[receivedDate.getMonth().toString()];
-      console.log(dateDifference);
 
-      }
-      else{
-        jobs.hasEnoughTime=false;
-        jobs.applybefore=dateDifference.toString();
-      }
-    }
    }
 
   ngOnInit() {
+    this.jobsSub=this.jobsService.jobs.subscribe(jobs=>{
+      this.jobs=jobs;
+      // for(let jobs of this.jobs){
+      //   let receivedDate=new Date(jobs.jobDeadline)
+      //   let timeDifference=receivedDate.getTime()-new Date().getTime();
+      //   let dateDifference=receivedDate.getDate()-new Date().getDate();
+      //   console.log(receivedDate,'r');
+      //   console.log(timeDifference,'td');
+      //   console.log(dateDifference,'dd');
+
+      //   if(timeDifference>604800000){
+      //     this.hasEnoughTime=true;
+      //   jobs.jobDeadline=new Date(receivedDate.getDate().toString()+" "+this.months[receivedDate.getMonth().toString()]);
+      //   console.log(jobs.jobDeadline,'1');
+
+
+      //   }
+      //   else{
+      //     this.hasEnoughTime=false;
+      //     jobs.jobDeadline=new Date(dateDifference.toString());
+      //     console.log(jobs.jobDeadline,'2');
+      //   }
+      // }
+    });
+  }
+  ionViewWillEnter(){
+    this.isLoading=true;
+    this.jobsService.fetchJobs().subscribe(()=>{
+      this.isLoading=false;
+    });
   }
   onSearchBarIconClick(){
     this.hasSearchBarOpen=true;
@@ -44,5 +64,10 @@ export class JobsPage implements OnInit {
   }
   onApply(jobId:string){
     this.router.navigateByUrl('/employee/jobs/'+jobId)
+  }
+  ngOnDestroy(): void {
+      if(this.jobsSub){
+        this.jobsSub.unsubscribe();
+      }
   }
 }
