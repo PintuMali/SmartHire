52583bab9c6eadabddc8b15b44703130d0d89bd9
Pintoo Mali <pintoomali05@gmail.com>
2023-01-18 22:19:04 +0530

    made job details dynamic

--- src/app/employee/jobs/job-details/job-details.page.ts
+++ src/app/employee/jobs/job-details/job-details.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
-import { take, tap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
+import { switchMap, take, tap } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
-import { User } from 'src/app/auth/user.model';
+import { Job } from '../../job.model';
+import { JobsService } from '../../jobs.service';
 
 @Component({
   selector: 'app-job-details',
@@ -11,10 +12,30 @@ import { User } from 'src/app/auth/user.model';
   styleUrls: ['./job-details.page.scss'],
 })
 export class JobDetailsPage implements OnInit {
+  job:Job;
+  isLoading=false;
 
-  constructor(private router:Router,private authService:AuthService,private alertCtrl:AlertController) { }
+  constructor(private route:ActivatedRoute,private navCtrl:NavController,private router:Router,private authService:AuthService,private alertCtrl:AlertController,private jobService:JobsService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap=>{
+      if(!paramMap.has('jobId')){
+        this.navCtrl.navigateBack('/employee/jobs');
+        return;
+      }
+      this.isLoading=true;
+       this.jobService.getJob(paramMap.get('jobId'))
+      .subscribe({next:job=>{
+        this.job=job
+        this.isLoading=false;
+      },error:error=>{
+        this.alertCtrl.create({header:'An error occured!',message:'Could not load job',buttons:[{text:'Okay',handler:()=>{
+          this.router.navigate(['/employee/jobs'])
+        }}]}).then(alerEl=>{
+          alerEl.present();
+        })
+      }});
+    })
   }
   onApplyClick(){
     // this.authService.userIsAuthenticated.pipe(take(1),tap(isAuhenticate=>{
